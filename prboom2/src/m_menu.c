2df2f6de7b51c23cdb01329159f8d02fd4fee4e4
TonyFideo <tonyfideo@users.noreply.github.com>
2026-09-21 03:33:31 -0300

    fix(vita): refine credits

--- prboom2/src/m_menu.c
+++ prboom2/src/m_menu.c
@@ -5904,8 +5904,8 @@ setup_menu_t cred_settings[]={
   {"Randy Heit for ZDOOM",S_SKIP|S_CREDIT|S_LEFTJUST,m_null, CR_X2, CR_Y + CR_S*(adcr+5)+CR_SH*cr_adcr},
   {"Michael 'Kodak' Ryssen for DOOMGL",S_SKIP|S_CREDIT|S_LEFTJUST,m_null, CR_X2, CR_Y + CR_S*(adcr+6)+CR_SH*cr_adcr},
   {"Jess Haas for lSDLDoom",S_SKIP|S_CREDIT|S_LEFTJUST,m_null, CR_X2, CR_Y + CR_S*(adcr+7) + CR_SH*cr_adcr},
-  {"FGSFDSFGS - ORIGINAL PRBOOM+ PORT",S_SKIP|S_CREDIT|S_LEFTJUST,m_null, CR_X2, CR_Y + CR_S*(adcr+8)+CR_SH*cr_adcr},
-  {"RINNEGATAMANTE - FOR VITAGL",S_SKIP|S_CREDIT|S_LEFTJUST,m_null, CR_X2, CR_Y + CR_S*(adcr+9)+CR_SH*cr_adcr},
+  {"FGSFDSFGS FOR ORIGINAL PRBOOM+ PORT",S_SKIP|S_CREDIT|S_LEFTJUST,m_null, CR_X2, CR_Y + CR_S*(adcr+8)+CR_SH*cr_adcr},
+  {"RINNEGATAMANTE FOR VITAGL",S_SKIP|S_CREDIT|S_LEFTJUST,m_null, CR_X2, CR_Y + CR_S*(adcr+9)+CR_SH*cr_adcr},
   {"all others who helped (see AUTHORS file)",S_SKIP|S_CREDIT|S_LEFTJUST,m_null, CR_X2, CR_Y + CR_S*(adcr+10)+CR_SH*cr_adcr},
 
   {0,S_SKIP|S_END,m_null}
